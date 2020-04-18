179053655bf2f43785cb7bd78f5c8f311099f285
maciejows <owsianny.maciej@gmail.com>
2020-04-18 18:45:49 +0200

    Dodano leaflet maps

--- src/app/components/advertisement-details/advertisement-details.component.ts
+++ src/app/components/advertisement-details/advertisement-details.component.ts
@@ -3,6 +3,8 @@ import { ActivatedRoute } from '@angular/router';
 import { DataService } from '../../services/data.service';
 import { Advertisement } from '../../models/advertisement';
 import { Location } from '@angular/common';
+import * as L from 'leaflet';
+import { OpenStreetMapProvider } from 'leaflet-geosearch';
 
 @Component({
   selector: 'app-advertisement-details',
@@ -10,7 +12,8 @@ import { Location } from '@angular/common';
   styleUrls: ['./advertisement-details.component.scss']
 })
 export class AdvertisementDetailsComponent implements OnInit {
-
+  private map;
+  private provider = new OpenStreetMapProvider();
   id: number;
   advert: Advertisement;
 
@@ -22,8 +25,29 @@ export class AdvertisementDetailsComponent implements OnInit {
 
   ngOnInit(): void {
     this.id = +this.activatedRoute.snapshot.paramMap.get('id');
-    this.dataService.getSingleAdvert(this.id).subscribe( (data) => this.advert = data['data']);
+    this.dataService.getSingleAdvert(this.id)
+    .subscribe( (data) => {
+      this.advert = data['data'];
+      this.initMap();
+    });
   }
+
+  private initMap(): void {
+    this.map = L.map('map', {
+      center: [ 52.84135, 17.71124 ],
+      zoom: 16
+    });
+    const tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 19,
+      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    });
+
+     tiles.addTo(this.map);
+
+     this.provider.search({query: "Polna 3 Żnin"}).then(
+       (res) => console.log(res)
+     );
+    }
 
   goBack(): void {
     this.location.back();
